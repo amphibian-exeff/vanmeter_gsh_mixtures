f95dc0c09152a421e51b76b94eea838ee0dd6ca6
Tom Purucker <purucker.tom@gmail.com>
2021-07-30 12:28:26 -0400

    heatmap figure updates

--- src/07final_heatmap.R
+++ src/07final_heatmap.R
@@ -8,7 +8,7 @@ dim(rvm_heatmap_peaks_all)
 colnames(rvm_heatmap_peaks_all)
 rownames(rvm_heatmap_peaks_all)
 
-rvm_heatmap_peaks_cols <- rvm_heatmap_peaks_all[,c(8:14)]
+rvm_heatmap_peaks_cols <- rvm_heatmap_peaks_all[,c(23:29)]
 rownames(rvm_heatmap_peaks_cols) <- rvm_heatmap_peaks_all$peak_id
 
 rvm_heatmap_peaks <- rvm_heatmap_peaks_cols[which(rowSums(rvm_heatmap_peaks_cols[1:3], na.rm=T)>0),]
@@ -17,25 +17,21 @@ rvm_heatmap_peaks <- rvm_heatmap_peaks_cols[which(rowSums(rvm_heatmap_peaks_cols
 View(rvm_heatmap_peaks)
 colnames(rvm_heatmap_peaks)
 
-rvm_heatmap_peaks$z_fvalue[which(is.na(rvm_heatmap_peaks$z_fvalue))] <- 0.9
-rvm_heatmap_peaks$l_fvalue[which(is.na(rvm_heatmap_peaks$l_fvalue))] <- 0.9
-rvm_heatmap_peaks$n_fvalue[which(is.na(rvm_heatmap_peaks$n_fvalue))] <- 0.9
-rvm_heatmap_peaks$zl_fvalue[which(is.na(rvm_heatmap_peaks$zl_fvalue))] <- 0.9
-rvm_heatmap_peaks$zn_fvalue[which(is.na(rvm_heatmap_peaks$zn_fvalue))] <- 0.9
-rvm_heatmap_peaks$ln_fvalue[which(is.na(rvm_heatmap_peaks$ln_fvalue))] <- 0.9
-rvm_heatmap_peaks$zln_fvalue[which(is.na(rvm_heatmap_peaks$zln_fvalue))] <- 0.9
+
 
 
 colnames(rvm_heatmap_peaks) <- c("Atrazine (Z)", "Alachlor (L)", "Urea (N)",
                                  "Z:L", "Z:N", "L:N", "Z:L:N")
 
 View(rvm_heatmap_peaks)
-pheatmap(as.matrix(-log(rvm_heatmap_peaks)), dendrogram="none", col = coul)
+pheatmap(as.matrix(-log(rvm_heatmap_peaks)), dendrogram="none", col = coul,  cluster_rows = TRUE,
+         cluster_cols = FALSE)
 
 # display.brewer.all()
 coul <- colorRampPalette(brewer.pal(8, "YlOrRd"))(18)
 heatmap_png <- pheatmap(as.matrix(-log(rvm_heatmap_peaks)), dendrogram="none", col = coul)
 png(file.path(rvm_graphics, "heatmap_mixtures_main_effects.png"),
-    width=5, height=8, units="in", res=300)
-  print(heatmap_png)
+    width=5, height=8, units="in", res=300  )
+  pheatmap(as.matrix(-log(rvm_heatmap_peaks)), dendrogram="none", col = coul,  cluster_rows = TRUE,
+         cluster_cols = FALSE)
 dev.off()
